5556c5c4e1e0b680d087565cc33002e0f9195304
Leonardo Bonacci <aabcehmu@mailfence.com>
2022-12-06 13:53:09 +1300

    generated code

---
+++ server/src/test/java/guru/bonacci/feign/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.bonacci.feign.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
